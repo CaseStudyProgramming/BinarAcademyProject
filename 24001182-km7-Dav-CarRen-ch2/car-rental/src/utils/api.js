70217ba2100587b2b1ffdcc205fc69d07dcbb969
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-09-27 18:53:23 +0700

    fix 404 no cars.json  error from deployment

--- 24001182-km7-Dav-CarRen-ch2/car-rental/src/utils/api.js
+++ 24001182-km7-Dav-CarRen-ch2/car-rental/src/utils/api.js
@@ -1,15 +1,10 @@
-//api.js
 export const fetchCarData = async () => {
   try {
-    const response = await fetch("/data/cars.json");
+    const response = await fetch("/data/cars.json"); // Remains the same, but file should be in the public folder
     if (!response.ok) {
       throw new Error("Failed to fetch car data");
     }
     const data = await response.json();
-
-    // Log fetched data for debugging
-    // console.log("Car data fetched:", data);
-
     return data;
   } catch (error) {
     console.error("Error fetching car data:", error);
