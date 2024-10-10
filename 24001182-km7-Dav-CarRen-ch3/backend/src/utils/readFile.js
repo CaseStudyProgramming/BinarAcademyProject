c4e823b88eb02ab208a409c2464a76338c765a13
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-10-11 01:42:32 +0700

    fix bug deploy

--- 24001182-km7-Dav-CarRen-ch3/backend/src/utils/readFile.js
+++ 24001182-km7-Dav-CarRen-ch3/backend/src/utils/readFile.js
@@ -1,10 +1,10 @@
-// root/src/utils/fileUtils.js
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
 
 const dataPath = path.join(process.cwd(), "public/data/cars.json");
 
 const readData = () => {
+  console.log("Trying to read file from path:", dataPath); // Log the file path
   const jsonData = fs.readFileSync(dataPath, "utf-8");
   return JSON.parse(jsonData);
 };
