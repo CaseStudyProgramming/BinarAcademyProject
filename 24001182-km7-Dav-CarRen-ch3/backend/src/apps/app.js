a64282203b3ca89d95c192779b4560eb28d68e67
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-10-10 21:13:14 +0700

    chapter3

---
+++ 24001182-km7-Dav-CarRen-ch3/backend/src/apps/app.js
@@ -0,0 +1,13 @@
+// src/apps/app.js
+const express = require("express");
+const fileUpload = require("express-fileupload"); // New import
+const carRoutes = require("../routers/carRoutes");
+const errorHandler = require("../middlewares/errorHandler");
+
+const app = express();
+app.use(express.json());
+app.use(fileUpload()); // Enable file upload handling
+app.use("/api", carRoutes);
+app.use(errorHandler);
+
+module.exports = app;
