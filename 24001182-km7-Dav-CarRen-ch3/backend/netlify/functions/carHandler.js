ebbb1564e571d35e03d4241b4e5dafb40cf13416
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-10-10 22:55:55 +0700

    doploy with netlify

---
+++ 24001182-km7-Dav-CarRen-ch3/backend/netlify/functions/carHandler.js
@@ -0,0 +1,18 @@
+// netlify/functions/carHandler.js
+require("dotenv").config();
+const serverless = require("serverless-http");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+
+const carRoutes = require("../../src/routers/carRoutes");
+const errorHandler = require("../../src/middlewares/errorHandler");
+
+const app = express();
+app.use(express.json());
+app.use(fileUpload());
+
+// Menggunakan rute yang sudah dibuat
+app.use("/api", carRoutes);
+app.use(errorHandler);
+
+module.exports.handler = serverless(app);
