da702d58ea3f2a3548a5ce286bba938fa538e2c1
CaseStudyProgramming <casestudyprogramming@gmail.com>
2024-10-11 02:42:41 +0700

    fix midd

--- 24001182-km7-Dav-CarRen-ch3/backend/src/middlewares/errorHandler.js
+++ 24001182-km7-Dav-CarRen-ch3/backend/src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
-// root/src/middlewares/errorHandler.js
 module.exports = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
+  console.error(err); // This will log the full error stack
   res.status(status).json({ status, message });
 };
