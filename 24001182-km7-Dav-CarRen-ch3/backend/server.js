805030e43e4fd3b148ff84d9eca2420c274bd92c
DavidCristianPS <121373831+CaseStudyProgramming@users.noreply.github.com>
2024-10-11 08:39:26 +0700

    Delete 24001182-km7-Dav-CarRen-ch3/backend directory

--- 24001182-km7-Dav-CarRen-ch3/backend/server.js
+++
@@ -1,9 +0,0 @@
-// root/server.js
-require("dotenv").config();
-
-const app = require("./src/apps/app");
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
--- 24001182-km7-Dav-CarRen-ch3/backend/src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-module.exports = (err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Internal Server Error";
-  console.error(err); // This will log the full error stack
-  res.status(status).json({ status, message });
-};
